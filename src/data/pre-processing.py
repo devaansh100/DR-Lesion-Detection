e5b7688cf75f7bb5067f73a138c9ba2fe35a71ff
devaansh100 <devaanshgupta100@gmail.com>
2021-06-28 15:07:11 +0530

    Finalise preprocessing and add dataset partition numbers

--- src/data/pre-processing.py
+++ src/data/pre-processing.py
@@ -12,50 +12,46 @@ import csv
 def get_images():
 	'''Gets image names from the images directory'''
 
-	os.chdir('/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/train_data_unzip/train_001/')
+	os.chdir('/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/train_data_unzip/train/')
+	# os.chdir('/Users/devaanshgupta/Desktop/PS-I/DR-Lesion-Detection/data/train/')
 	images = os.listdir() #starting from 1 to avoid the .DS_Store
 	return images
 
-def get_images_to_augment(label):
-	'''Gets image names of images to augment with the passed label. Also returns the number of augmentations per image'''
+def get_images_to_augment():
+	'''Gets image names of images to augment with the passed label'''
 	# file = open('/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/trainLabels.csv')
 	file = open('/Users/devaanshgupta/Downloads/trainLabels.csv')
 	entries = file.read().split('\n')
 	entries = [entry.split(',') for entry in entries]
 	labels = [entries[x][1] for x in range(1, len(entries)-1)]
 	nImages,_,_ = plt.hist(labels, 5, facecolor='blue', alpha = 0.5)
-	# Label distribution for reference
-	# [25810.,  2443.,  5292.,   708.,   873.]
-	images = []
-	for i in range(1, len(entries) - 1):
-		if entries[i][1] == label:
-			images.append(entries[i][1])
-			if len(images) == nImages[label]:
-				break
 
-	return images, nAugmentations
+	images = {0: [], 1: [], 2: [], 3: [], 4: []}
+	for i in range(1, len(entries) - 1):
+		label = entries[i][1]
+		images[label].append(entries[i][0])
+
+	return images
 
 
 
 def mask(img):
 	'''Creates a mask for the image to remove outer black borders'''
 
+	#Converting from BGR to RGB
+	maskedImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
 	#Resizing the image
-	rows, _ = img.shape
-	scale = 600/rows if rows > 600 else rows/600
-	maskedImg = cv2.resize(img, None, fx = scale, fy = scale, interpolation = cv2.INTER_CUBIC)
+	maskedImg = cv2.resize(maskedImg, (224,224))
 
 	#Creating the mask. Remove black pixels and change to white pixels
-	maskedImg = np.where(maskedImg < 30, 255, maskedImg)
-
-	# #Choosing only the green channel
-	# maskedImg = maskedImg[:,:,2]
+	maskedImg = cv2.addWeighted(maskedImg,4, cv2.GaussianBlur(maskedImg , (0,0) , 30) ,-4 ,128)
 
 	return maskedImg
 
 def crop_edges(img):
 	'''Crops the extra edges in the image'''
-	rows, cols = img.shape
+	rows, cols, _ = img.shape
 
 	#Finding the color of the edges
 	borderColor = img[0,0]
@@ -66,22 +62,22 @@ def crop_edges(img):
 
 	#Finding the edges where the fundus image starts
 	for col in range(0, cols):
-		if not np.all(img[:,col] == borderColor):
+		if not np.all(img[:,col, :] == borderColor):
 			left = col - 5
 			break
 
 	for col in range(0, cols):
-		if not np.all(img[:,-col] == borderColor):
+		if not np.all(img[:,-col, :] == borderColor):
 			right = cols - (col - 5)
 			break
 
 	for row in range(0, rows):
-		if not np.all(img[row,:] == borderColor):
+		if not np.all(img[row,:, :] == borderColor):
 			top = row - 5
 			break
 
 	for row in range(0, rows):
-		if not np.all(img[-row,:] == borderColor):
+		if not np.all(img[-row,:, :] == borderColor):
 			bottom = rows - (row - 5)
 			break
 
@@ -96,7 +92,7 @@ def crop_edges(img):
 		bottom = rows
 
 	#Cropping the images to remove extra edges
-	img = img[top:bottom, left:right]
+	img = img[top:bottom, left:right, :]
 
 	return img
 
@@ -183,7 +179,11 @@ def enhance(img):
 	'''Enhances the image'''
 	#Enhancing the contrast
 	clahe = cv2.createCLAHE(clipLimit = 3)
-	imgNorm = clahe.apply(img)
+	imgNormR = clahe.apply(img[:,:,0])
+	imgNormG = clahe.apply(img[:,:,1])
+	imgNormB = clahe.apply(img[:,:,2])
+
+	imgNorm = np.stack((imgNormR, imgNormG, imgNormB), axis = -1)
 	return imgNorm
 
 def preprocess(image):
@@ -194,7 +194,7 @@ def preprocess(image):
 
 	#Read image into a numpy array
 	try:
-		img = cv2.imread(image, -1)[:,:,1]
+		img = cv2.imread(image, -1)
 	except:
 		print(f'Reading error in image {image}')
 		return 0
@@ -205,12 +205,12 @@ def preprocess(image):
 	#Cropping the extra edges
 	img = crop_edges(img)
 
-	#Cropping the extra edges
-	img = enhance(img)
+	#Add condition for image of size zero
 
 	#save image
 	try:
-		cv2.imwrite('/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/train_data_unzip/preprocessed_001/' + image, img)
+		cv2.imwrite('/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/train_data_unzip/preprocessed/' + image, img)
+		# cv2.imwrite('/Users/devaanshgupta/Desktop/PS-I/DR-Lesion-Detection/data/preprocessed/' + image, img)
 	except:
 		print(f'Writing error in image {image}')
 
@@ -250,8 +250,8 @@ def augment(images, nAugmentations):
 				new_image = cv2.warpAffine(image, rot_mat, (col,row))
 
 			#Convert to HSV 
-			# elif option == 3:
-			# 	imgHSV = cv2.cvtColor()
+			elif option == 3:
+				new_image = enhance(img)
 
 			try:
 				#Saving the augmented image
@@ -261,23 +261,25 @@ def augment(images, nAugmentations):
 
 
 def main():
-	'''Creates a process for each image'''
+	'''Creates a thread for each image'''
 	with concurrent.futures.ThreadPoolExecutor() as executor:
 		images = get_images()
-		
-		# results = list(tqdm(executor.map(preprocess, images), total=len(images)))
-		imagesToAugment, nAugmentations = get_images_to_augment()
+
+		results = list(tqdm(executor.map(preprocess, images), total=len(images)))
+		# imagesToAugment = get_images_to_augment()
 
 		#Image augmentation and pre-processing has been separated in order to understand the distribution after removing the poor quality images
-		augmented = executor.map(augment, imagesToAugment, nAugmentations)
+		# augmented = executor.map(augment, imagesToAugment, nAugmentations)
 	# images = get_images()
 	# preprocess(images[-1])
 
-	# Step 1: Categorise the images as good and poor and remove the poor images
+	# Step 1: Categorise the images as good and poor and remove the poor images(should we do this? I mean the model is based on the fact that we want to work with poor quality images as well). If yes, how?
 	# Step 2: Find the distribution again
 	# Step 3: Get 15000 images which don't have DR, 15000 images which do have DR - augment each grade accordingly. If the number required is too high, add more augmentation methods
 	# Step 4: Finalise the size and the number of channels that would be required, modify each code accordingly
-
+	# Step 5: Ensure that the division into training and validation is done correctly
+	# Step 6: I was going to use Resnet but not many people have really used that one. Going with the efficient net model - need 3 color channels and a specific size. Aso, will probably have to retrain that model since pre-trained isnt available. Will check if I should use weights from imagenet or not
+	# Step 7: The pre-processing will have to be done as per this, also adding gaussian noise to all colour channels was giving good sitnct features.
 		
 if __name__ == '__main__':
 	main()
