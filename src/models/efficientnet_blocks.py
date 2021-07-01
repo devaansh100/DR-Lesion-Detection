de065bf67e53d57a2b87013dba6a136eeec97bb5
devaansh100 <devaanshgupta100@gmail.com>
2021-07-01 12:00:07 +0530

    Remove unnecessary math import

--- src/models/efficientnet_blocks.py
+++ src/models/efficientnet_blocks.py
@@ -2,7 +2,6 @@ import torch
 from torch import Tensor
 import torch.nn as nn
 import torch.nn.functional as F
-from math import ceil
 
 class StandardCNNBlock:
 	def __init__(self, kernel_size, in_channels, out_channels, stride):
