8250e9cfc67b9a64758b6ad6d4fe1bbd43feac4f
devaansh100 <devaanshgupta100@gmail.com>
2021-07-02 18:08:01 +0530

    Add model path variable

--- src/models/train.py
+++ src/models/train.py
@@ -16,7 +16,8 @@ def main():
 	LEARNING_RATE = 1e-3
 	DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 	TRAIN_IMG = '/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/train_data_unzip/train_preprocessed/'
-	VAL_IMG = '/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/train_data_unzip/validation_preprocessed'
+	VAL_IMG = '/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/train_data_unzip/validation_preprocessed/'
+	MODEL_PATH = '/Users/devaanshgupta/Desktop/PS-I/DR-Lesion-Detection/models/'
 
 	training = DRDataset('/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/trainingLabels.csv', TRAIN_IMG, transforms.ToTensor())
 	validation = DRDataset('/Volumes/Seagate Backup Plus Drive/DR Kaggle Dataset/validationLabels.csv', VAL_IMG, transforms.ToTensor())
@@ -40,9 +41,10 @@ def main():
 			labels = labels.to(DEVICE)
 
 			predictions = model(images)
-			# print(predictions)
-			predictions = predictions.view(BATCH_SIZE, 5)
-			labels = labels.view(BATCH_SIZE)
+
+			batch_number = predictions.shape[0]
+			predictions = predictions.view(batch_number, 5)
+			labels = labels.view(batch_number)
 			# print(predictions)
 			# print(labels)
 
@@ -58,12 +60,16 @@ def main():
 		valid_loss = 0.0
 		model.eval()	
 		for batch_idx, (images, labels) in enumerate(tqdm(validation_loader)):
+			images = images.to(DEVICE)
+			labels = labels.to(DEVICE)
+
 			predictions = model(images)
 
+			batch_number = predictions.shape[0]
 			predictions = predictions.view(BATCH_SIZE, 5)
 			labels = labels.view(BATCH_SIZE)
 
-			loss = loss_fn(images, labels)
+			loss = loss_fn(predictions, labels)
 
 			valid_loss += loss.item() * images.size(0)
 
@@ -71,7 +77,7 @@ def main():
 
 		if min_valid_loss > valid_loss:
 			print(f'Validation Loss decreased from {min_valid_loss:.6f} to {valid_loss:.6f} \t Saving Model')
-			torch.save(model.dicts(), 'model.pth')
+			torch.save(model.state_dict(), MODEL_PATH + 'model.pth')
 			min_valid_loss = valid_loss
 if __name__ == '__main__':
 	main()
