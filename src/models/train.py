da3265630b5500bb82e2be7d643cfec5723d8916
devaansh100 <devaanshgupta100@gmail.com>
2021-07-06 03:32:40 +0530

    Add oversampling to manage class imbalance

--- src/models/train.py
+++ src/models/train.py
@@ -2,7 +2,7 @@ import torch
 import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 from dataset import DRDataset
 from tqdm import tqdm
 from efficientnet import EfficientNet
@@ -10,6 +10,7 @@ import numpy as np
 import torch.nn as nn
 import yaml
 import torch.nn.functional as F
+import pandas as pd
 
 def weights_init(m):
 	if isinstance(m, nn.Conv2d):
@@ -34,9 +35,14 @@ def train():
 	training = DRDataset(config['TRAIN_LABELS'], config['TRAIN_IMG'], transforms.Compose([transforms.ToTensor(), transforms.Resize(model.input_size)]))
 	validation = DRDataset(config['VALID_LABELS'], config['VAL_IMG'], transforms.Compose([transforms.ToTensor(), transforms.Resize(model.input_size)]))
 
-	training_loader = DataLoader(dataset = training, batch_size = config['BATCH_SIZE'], shuffle = True, num_workers = config['NUM_WORKERS'])
-	validation_loader = DataLoader(dataset = validation, batch_size = config['BATCH_SIZE'], shuffle = True, num_workers = config['NUM_WORKERS'])
+	class_weights = torch.tensor([1.0, 52.8, 48.75, 49.22, 52.01])
+	labels = torch.tensor(training.labels.iloc[:,1]).view(1, training.__len__())
+	sample_weights = torch.tensor(class_weights[labels])
 
+	sampler = WeightedRandomSampler(sample_weights, num_samples = training.__len__(), replacement = True)
+	
+	training_loader = DataLoader(dataset = training, batch_size = config['BATCH_SIZE'], num_workers = config['NUM_WORKERS'], sampler = sampler)
+	validation_loader = DataLoader(dataset = validation, batch_size = config['BATCH_SIZE'], shuffle = True, num_workers = config['NUM_WORKERS'])
 	min_valid_loss = np.inf
 
 	for epoch in range(config['NUM_EPOCHS']):
@@ -48,7 +54,7 @@ def train():
 		for batch_idx, (images, labels) in enumerate(tqdm(training_loader)):
 			images = images.to(DEVICE)
 			labels = labels.to(DEVICE)
-
+			
 			predictions = model(images)
 
 			batch_number = predictions.shape[0]
@@ -62,8 +68,8 @@ def train():
 			correct += correct_predictions
 
 			loss = loss_fn(predictions, labels)
-
 			optimizer.zero_grad()
+
 			loss.backward()
 
 			optimizer.step()
